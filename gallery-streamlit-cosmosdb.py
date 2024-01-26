1eca8cc4ace4ea052180a9c0a55a69536d5cae9f
omalinow248 <136571204+omalinow248@users.noreply.github.com>
2024-01-26 18:58:31 +0100

    Update gallery-streamlit-cosmosdb.py

--- gallery-streamlit-cosmosdb.py
+++ gallery-streamlit-cosmosdb.py
@@ -1,43 +1,11 @@
 import streamlit as st
 from dotenv import load_dotenv
 from azure.storage.blob import BlobServiceClient
-from azure.cosmos import CosmosClient, PartitionKey
 import os
-import datetime
-import uuid
+from io import BytesIO
 
 # Ładowanie zmiennych środowiskowych z pliku .env
 load_dotenv()
-
-# Funkcja do połączenia z bazą danych Cosmos DB
-def connect_to_cosmos_db():
-    cosmos_uri = os.getenv("COSMOS_URI")
-    cosmos_key = os.getenv("COSMOS_KEY")
-    client = CosmosClient(cosmos_uri, cosmos_key)
-    database_name = os.getenv("COSMOS_DATABASE_NAME")
-    database = client.get_database_client(database_name)
-    container_name = os.getenv("COSMOS_CONTAINER_NAME")
-    container = database.get_container_client(container_name)
-    return container
-
-# Funkcja do dodawania metadanych zdjęcia do bazy danych Cosmos DB
-def add_photo_metadata_to_cosmos(photo_name, description):
-    container = connect_to_cosmos_db()
-    # Generowanie unikalnego identyfikatora UUID
-    unique_id = str(uuid.uuid4())
-    item = {
-        'id': unique_id,  # Dodanie unikalnego ID
-        'photo_name': photo_name,
-        'description': description,
-        'upload_date': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    }
-    container.create_item(item)
-
-# Funkcja do pobierania metadanych zdjęć z bazy danych Cosmos DB
-def get_photo_metadata_from_cosmos():
-    container = connect_to_cosmos_db()
-    items = list(container.read_all_items())
-    return items
 
 # Funkcja do łączenia z kontem Blob Storage
 def connect_to_azure_storage():
@@ -50,66 +18,59 @@ def list_files(container_name, blob_service_client):
     container_client = blob_service_client.get_container_client(container_name)
     blobs_list = container_client.list_blobs()
     for blob in blobs_list:
-        blob_client = container_client.get_blob_client(blob.name)
-        blob_data = blob_client.download_blob().readall()
-        st.image(blob_data, caption=blob.name)
-        photo_metadata = get_photo_metadata_from_cosmos()
-        for item in photo_metadata:
-            if item['photo_name'] == blob.name:
-                st.write("Opis:", item['description'])
-                st.write("Data dodania:", item['upload_date'])
+        st.write(f"- {blob.name}")
     return blobs_list
 
-#Funkcja do dodawania nowego zdjęcia
-def add_new_photo(blob_service_client, container_name):
-    st.header("Dodaj nowe zdjęcie:")
-    uploaded_file = st.file_uploader("Wybierz plik", type=["jpg", "jpeg", "png"])
-    description = st.text_input("Dodaj opis zdjęcia:")
+# Funkcja do dodawania nowego pliku
+def add_new_file(blob_service_client, container_name):
+    st.header("Dodaj nowy plik:")
+    uploaded_file = st.file_uploader("Wybierz plik", type=["pdf", "docx", "txt"])
     if uploaded_file is not None:
-        st.image(uploaded_file, caption="Załadowane zdjęcie", use_column_width=True)
+        st.write("Nazwa pliku:", uploaded_file.name)
         if st.button("Załaduj"):
             file_contents = uploaded_file.read()
             container_client = blob_service_client.get_container_client(container_name)
             blob_client = container_client.get_blob_client(uploaded_file.name)
             blob_client.upload_blob(file_contents, overwrite=True)
-            add_photo_metadata_to_cosmos(uploaded_file.name, description)
-            st.success("Zdjęcie zostało załadowane!")
+            st.success("Plik został załadowany!")
 
-# Funkcja do aktualizacji rekordu w bazie danych Cosmos DB po usunięciu zdjęcia
-def update_photo_metadata_in_cosmos(photo_name):
-    container = connect_to_cosmos_db()
-    items = list(container.read_all_items())
-    for item in items:
-        if item['photo_name'] == photo_name:
-            item['status'] = "deleted"  # Dodanie nowego pola "status" z wartością "deleted"
-            container.upsert_item(item)
+# Funkcja do pobierania pliku
+def download_file(container_name, blob_service_client):
+    container_client = blob_service_client.get_container_client(container_name)
+    blobs_list = container_client.list_blobs()
 
-# Funkcja do usuwania zdjęć
+    selected_file = st.selectbox("Wybierz plik do pobrania:",
+                                 [blob.name for blob in blobs_list])
+    if st.button("Pobierz"):
+        blob_client = container_client.get_blob_client(selected_file)
+        stream = BytesIO()
+        blob_client.download_blob().download_to_stream(stream)
+        st.download_button(label="Pobierz plik", data=stream.getvalue(), file_name=selected_file)
+        
+# Funkcja do usuwania plików
 def delete_file(container_name, blob_service_client):
     container_client = blob_service_client.get_container_client(container_name)
     blobs_list = container_client.list_blobs()
 
-    selected_file = st.selectbox("Wybierz zdjęcie do usunięcia:",
+    selected_file = st.selectbox("Wybierz plik do usunięcia:",
                                  [blob.name for blob in blobs_list])
     if st.button("Usuń"):
         blob_client = container_client.get_blob_client(selected_file)
         blob_client.delete_blob()
-        update_photo_metadata_in_cosmos(selected_file)  # Aktualizacja danych w Cosmos DB
-        st.success(f"Zdjęcie {selected_file} zostało usunięte!")
+        st.success(f"Plik {selected_file} został usunięty!")
         st.experimental_rerun()
 
 # Funkcja do obsługi interfejsu aplikacji Streamlit
 def main():
     blob_service_client = connect_to_azure_storage()
-    container_name = os.getenv("CONTAINER_NAME")
-    
-    st.title("Galeria Zdjęć z Blob Storage")
-    add_new_photo(blob_service_client, container_name)
-    
-    st.header("Wszystkie zdjęcia:")
+    container_name = os.getenv("CONTAINER_NAME") 
+    st.title("System do archiwizacji dokumentów")
+    add_new_file(blob_service_client, container_name)
+    st.header("Lista plików:")
     list_files(container_name, blob_service_client)
-
-    st.header("Usuń zdjęcie:")
+    st.header("Pobierz plik:")
+    download_file(container_name, blob_service_client)
+    st.header("Usuń plik:")
     delete_file(container_name, blob_service_client)
 
 if __name__ == "__main__":
